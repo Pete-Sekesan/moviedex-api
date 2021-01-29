3b6e52ca9f89939883214f66097e6abc33f3cf3e
Peter Sekesan <psekesan@gmail.com>
2021-01-29 16:11:54 -0500

    server up and running

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+const PORT = 8000;
+const MOVIEDEX = ('./moviedex')
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+
+
+
+
+
+
+
+app.listen( PORT, () => {
+console.log(`Server is running at http://localhost:${PORT}`)
+})
