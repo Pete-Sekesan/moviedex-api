8322b3d92224c932c7b905ace167149cd78cf364
Peter Sekesan <psekesan@gmail.com>
2021-01-29 20:25:03 -0500

    api token, moviedex json update

--- server.js
+++ server.js
@@ -11,8 +11,17 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
 
+app.get('/movie')
 
+function validateBearerToken(req, res, next) {
+  const authToken = req.get("Authorization");
+  const apiToken = process.env.API_TOKEN;
 
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized Request" });
+  }
+  next();
+}
 
 
 
