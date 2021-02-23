223f0fcf623894c2d8df086f75ce6d3b2153f0f6
Peter Sekesan <psekesan@gmail.com>
2021-02-23 13:48:43 -0500

    fixed server.js for assignment

--- server.js
+++ server.js
@@ -1,64 +1,71 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-require("dotenv").config(); 
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIES = require("./movies-small.json");
 
 const app = express();
-const PORT = 8000;
-const MOVIEDEX = ('./moviedex')
 
-app.use(morgan('dev'));
-app.use(validateBearerToken);
-app.use(cors());
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
 app.use(helmet());
+app.use(cors());
 
-app.get('/movie', handleGetMovie)
-
-function validateBearerToken(req, res, next) {
-  const authToken = req.get("Authorization");
+app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
 
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    return res.status(401).json({ error: "Unauthorized Request" });
+  if (!authToken || authToken !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
   }
+
   next();
-}
+});
 
-function handleGetMovie(req,res) {
-  let response = MOVIEDEX;
-  //genre query
-  if (req.query.genre) {
-    response = response.filter((movie) => {
-      return movie.genre.toLowerCase().includes(req.query.genre.toLowerCase());
-    });
+app.get("/movie", function handleGetMovie(req, res) {
+  let response = MOVIES;
+  console.log(response);
+
+  if (req.query.name) {
+    response = response.filter((MOVIES) =>
+      MOVIES.film_title.toLowerCase().includes(req.query.name.toLowerCase())
+    );
   }
 
-  //country query
+  if (req.query.genre) {
+    response = response.filter((MOVIES) =>
+      MOVIES.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
 
   if (req.query.country) {
-    response = response.filter((movie) =>
-      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    response = response.filter((MOVIES) =>
+      MOVIES.country.toLowerCase().includes(req.query.country.toLowerCase())
     );
   }
-  
-  //average vote query
-    
+
   if (req.query.avg_vote) {
     response = response.filter(
-      (movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+      (MOVIES) => Number(MOVIES.avg_vote) >= Number(req.query.avg_vote)
     );
   }
 
+  app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === "production") {
+      response = { error: { message: "server error" } };
+    } else {
+      response = { error };
+    }
+    res.status(500).json(response);
+  });
+
   res.json(response);
-}
+});
 
+const PORT = process.env.PORT || 8080;
 
-
-
-
-
-
-app.listen( PORT, () => {
-console.log(`Server is running at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
